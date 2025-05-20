23d5f814ea824de315623f1f57afab471b4c3518
Abdoul-ka <boireabdoulkarim353@gmail.com>
2025-05-20 11:20:27 +0000

    première sauvegarde première sauvegardpremière sauvegardee# Please enter the commit message for your changes. Lines starting

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello world !!!");
+console.log(4 + 4);
+
+let Myvariable = 10;
+console.log("My variable is =", Myvariable);
+
+const origin = "Mali";
+console.log("origine", origin);
+mqj = origin.toUpperCase();
+console.log(mqj);
